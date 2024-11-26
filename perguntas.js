e31909d3db2e4315b9da4a46bce1877ac8d474f2
Caroline Saavedra <00001114810277sp@al.educacao.sp.gov.br>
2024-11-26 05:14:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    'O que é energia cinética',
+    'É a energia associada ao movimento de um corpo que depende da massa e da velocidade'
+)
+
+criaCartao(
+    'Química',
+    'O que é a tabela periódica',
+    'É uma organização dos elementos químicos com base em suas propriedades e números atômicos'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a função do DNA nas células',
+    'Armazenar informações genéticas e controlar as funções celulares por meio da produção de proteínas'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é uma equação do segundo grau',
+    'É uma equação na forma ax2 mais bx mais c igual a zero onde a b e c são números reais e a é diferente de zero'
+)
